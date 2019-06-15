fbc0bea1c0f63fb37ab24dea96ec575f1ed52500
PhillHenry <phillhenry@gmail.com>
2019-06-15 10:12:47 +0100

    Works with Python 3 now.

--- ueba/gen/logins.py
+++ ueba/gen/logins.py
@@ -9,13 +9,15 @@ def data(n, t):
 
 if __name__ == "__main__":
     people = 100
-    days = 100
+    days = 365
     d = data(people, days)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    xs = range(days)
-    ys = range(people)
-    X, Y = np.meshgrid(xs, ys)  # `plot_surface` expects `x` and `y` data to be 2D
-    ax.plot_surface(X, Y, data)
+    x = range(people)
+    y = range(days)
+
+    hf = plt.figure()
+    ha = hf.add_subplot(111, projection='3d')
+
+    X, Y = np.meshgrid(x, y)  # `plot_surface` expects `x` and `y` data to be 2D
+    ha.plot_surface(X.T, Y.T, d)
 
     plt.show()
