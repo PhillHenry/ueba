2345bab2dace68c76f24685fe638e730b81f4ac3
PhillHenry <phillhenry@gmail.com>
2021-01-04 11:08:37 +0000

    Cleaned up a bit but still experimenting.

--- ueba/gen/2DFFT.py
+++ ueba/gen/2DFFT.py
@@ -1,19 +1,32 @@
-from scipy.fft import ifftn
+from scipy.fft import ifftn, fftn
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
 
+
 # from https://docs.scipy.org/doc/scipy/reference/tutorial/fft.html
+def data():
+    xf = np.zeros((N,N))
+    event = 1.
+    period = 7
+    offset = 1
+    xf[offset, :N] = event
+    xf[offset + period, :N] = event
+    xf[offset + 2 * period, :N] = event
+    xf[offset + 3 * period, :N] = event
+    xf[offset + 4 * period, :N] = event
+    return xf
+
 N = 30
-f, (ax1, ax4) = plt.subplots(2, 1, sharex='col', sharey='row')
-xf = np.zeros((N,N))
-event = 1.
-xf[0, :N] = event
-xf[7, :N] = event
-xf[14, :N] = event
-xf[21, :N] = event
-xf[28, :N] = event
+f, (ax1, ax2, ax3) = plt.subplots(3, 1)
+xf = data()
+
 Z = ifftn(xf)
 ax1.imshow(xf, cmap=cm.Reds)
-ax4.imshow(np.real(Z), cmap=cm.gray)
+ax2.imshow(np.real(Z), cmap=cm.gray)
+d = np.real(Z)
+d -= d.mean(0)
+dt = 0.1
+fr = np.fft.fftfreq(N,dt)
+ax3.plot(np.fft.fftshift(fr), np.abs(d))
 plt.show()
