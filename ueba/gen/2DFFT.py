1a518ee4ac29098a46d2df519f99aa6bc2e8ad2d
PhillHenry <phillhenry@gmail.com>
2021-01-04 11:32:34 +0000

    More versatile generation of data.

--- ueba/gen/2DFFT.py
+++ ueba/gen/2DFFT.py
@@ -5,22 +5,19 @@ import numpy as np
 
 
 # from https://docs.scipy.org/doc/scipy/reference/tutorial/fft.html
-def data():
-    xf = np.zeros((N,N))
+def data(N):
+    xf = np.random.rand(N, N)
     event = 1.
-    period = 7
+    period = 4
     offset = 1
-    xf[offset, :N] = event
-    xf[offset + period, :N] = event
-    xf[offset + 2 * period, :N] = event
-    xf[offset + 3 * period, :N] = event
-    xf[offset + 4 * period, :N] = event
+    for i in range(N // period):
+        xf[offset + (i * period), :N] = event
     return xf
 
 
 N = 30
 f, (ax1, ax2, ax3) = plt.subplots(3, 1)
-xf = data()
+xf = data(N)
 
 xf -= xf.mean()
 Z = np.fft.fftn(xf)
