7206366f7052e79c626badc74e3e1c70287b454f
PhillHenry <phillhenry@gmail.com>
2021-01-04 13:53:48 +0000

    More noise.

--- ueba/gen/2DFFT.py
+++ ueba/gen/2DFFT.py
@@ -2,22 +2,26 @@ from scipy.fft import ifftn, fftn
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+import random as r
 
 
 # from https://docs.scipy.org/doc/scipy/reference/tutorial/fft.html
-def data(N):
+def data(N, noise=0):
     xf = np.random.rand(N, N)
     event = 1.
     period = 8
     offset = 1
     for i in range(N // period):
-        xf[offset + (i * period), :N] = event
+        row_idx = offset + (i * period)
+        xf[row_idx, :N] = event
+        for _ in range(N):
+            xf[row_idx, r.randint(0, N - 1)] = 0
     return xf
 
 
 N = 40
 f, (ax1, ax2, ax3) = plt.subplots(3, 1)
-xf = data(N)
+xf = data(N, 3)
 
 xf -= xf.mean()
 Z = np.fft.fftn(xf)
