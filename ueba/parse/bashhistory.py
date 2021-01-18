2321acd8f7fd17abf83b61050c5182d266c29736
Phillip Henry <PhillHenry@gmail.com>
2021-01-18 12:07:39 +0000

    A very poorly performing VAE.

--- ueba/parse/bashhistory.py
+++ ueba/parse/bashhistory.py
@@ -1,4 +1,11 @@
 import sys
+import ueba.gen.vae_periodicity as vae
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Dense
+from keras.optimizers import Adam
+import numpy as np
+import pylab as plt
 
 
 def index_words(lines, word_index={}):
@@ -33,6 +40,54 @@ def truncate_or_pad(vs, n):
     return sized
 
 
+def read(file):
+    lines = []
+    for line in open(file, "r"):
+        lines.append(line)
+    return lines
+
+
+def create_model(n):
+    m = Sequential()
+    m.add(Dense(n,      activation='elu', input_shape=(n,)))
+    m.add(Dense(n//2,   activation='elu'))
+    m.add(Dense(2,      activation='linear', name="bottleneck"))
+    m.add(Dense(n//2,   activation='elu'))
+    m.add(Dense(n,      activation='elu'))
+    m.add(Dense(n,      activation='sigmoid'))
+    m.compile(loss='mean_squared_error', optimizer=Adam())
+    return m
+
+
+def train(vectors):
+    vec_length = len(vectors[0])
+    vectors = np.vstack(vectors)
+    m = create_model(vec_length)
+    train_size = int(len(vectors) * 0.5)
+    x_train = vectors[:train_size]
+    x_test = vectors[train_size:]
+    print("Train size = {}, test size = {}, vector length = {}".format(len(x_train), len(x_test), vec_length))
+    history = m.fit(x_train, x_train, batch_size=4, epochs=200, verbose=1, validation_data=(x_test, x_test))
+    encoder = Model(m.input, m.get_layer('bottleneck').output)
+    return history, encoder
+
+
+def run(lines1, lines2):
+    vec_length = 16
+
+    dictionary = index_words(lines1)
+    dictionary = index_words(lines2, dictionary)
+    vectors1 = truncate_or_pad(vectorize(lines1, dictionary), vec_length)
+    vectors2 = truncate_or_pad(vectorize(lines2, dictionary), vec_length)
+
+    history, encoder = train(vectors1)
+    vae.plot_loss(history)
+
+    plt.show()
+
+
 if __name__ == "__main__":
-    dictionary = index_words(open(sys.argv[1], "r"))
-    dictionary = index_words(open(sys.argv[2], "r"), dictionary)
+    xs = read(sys.argv[1])
+    ys = read(sys.argv[2])
+    run(xs, ys)
+
