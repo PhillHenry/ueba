ad751f2adcae88b145991df4efd80ac54270bd62
Phillip Henry <PhillHenry@gmail.com>
2021-01-23 10:18:55 +0000

    Attempting to plot.

--- ueba/parse/syslog.py
+++ ueba/parse/syslog.py
@@ -2,6 +2,9 @@ import time
 import datetime
 import sys
 import ueba.parse.files as fi
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 
 class SyslogEntry:
@@ -17,7 +20,7 @@ def parse(filename):
         items = line.split(" ")
         date = " ".join(items[:3])
         epoch = parse_to_epoch(date)
-        label = items[3]
+        label = items[4]
         entry = SyslogEntry(epoch, label)
         parsed.append(entry)
     return parsed
@@ -45,11 +48,42 @@ def vectorize(logs):
     points = []
     for x in logs:
         if label_to_index.get(x.label) is None:
+            print(x.label)
             label_to_index[x.label] = label_counter
             label_counter += 1
         points.append([label_to_index[x.label], x.time])
     return points
 
 
+def as_matrix(filename, max_y=200):
+    parsed = parse(filename)
+    points = vectorize(parsed)
+    max_time = max(map(lambda x: x[1], points))
+    labels = set(map(lambda x: x[0], points))
+    max_x = len(labels)
+
+    print("number of points = {}, max_time = {}, labels = {}".format(len(points), max_time, labels))
+
+    raw = np.zeros([max_x, max_y + 1])
+
+    for pt in points:
+        y = int((pt[1] / max_time) * max_y)
+        x = pt[0]
+        raw[x, y] = raw[x, y] + 1
+
+    return raw
+
+
+def fourier_of(filename):
+    raw = as_matrix(filename)
+    raw -= raw.mean()
+    Z = np.fft.fftn(raw)
+    frequencies = np.fft.fftshift(np.abs(Z))
+    # f, (ax1, ax2) = plt.subplots(2, 1)
+
+    plt.imshow(raw, cmap=cm.Reds)
+    plt.show()
+
+
 if __name__ == "__main__":
-    parse(sys.argv[1])
+    fourier_of(sys.argv[1])
