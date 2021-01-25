15802892f6fb14436604f941a8684a49562e61e4
Phillip Henry <PhillHenry@gmail.com>
2021-01-25 14:04:13 +0000

    3d plot of points.

--- ueba/cluster_view.py
+++ ueba/cluster_view.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import DBSCAN
 import string
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def outliers(coords, eps=0.4, min_samples=20):
@@ -22,6 +24,24 @@ def find_outliers_in(data):
     print("0-based lines of outliers = {}".format(outlier_lines))
 
 
+def plot_3d(data):
+    """
+    :param data: one dimension of index, 3 of co-ordinates. Assumes 2 classes of equal size and sequential in array.
+    """
+    n = np.shape(data)[0] - 1
+    fig = pyplot.figure()
+    fig.set_tight_layout(False)
+    ax = Axes3D(fig)
+    cs = np.zeros(n, dtype=int)
+    cs[:n//2] = 0
+    cs[n//2:] = 1
+    colors = np.array(["red", "green"])
+    ax.scatter(data[1:, 1], data[1:, 2], data[1:, 3], c=colors[cs])
+    pyplot.show()
+
+
 if __name__ == "__main__":
     my_data = np.genfromtxt(sys.argv[1], delimiter=',')
     find_outliers_in(my_data)
+    if np.shape(my_data)[1] == 4:
+        plot_3d(my_data)
